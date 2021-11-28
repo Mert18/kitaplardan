b5eeb6a41d8b0bf5468f9e395fb4c5b2a186c415
Mert18 <mertuygur02@gmail.com>
2021-11-29 00:53:06 +0300

    başlar ay doğarken saltanatı sultan-ı yegahın

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.REACT_APP_SERVER_URI,
   cache: new InMemoryCache(),
 });
 
